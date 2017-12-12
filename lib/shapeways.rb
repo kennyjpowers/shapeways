7233c39e3410877dc85e845bae02b585ab1e44f3
Kenneth Priester <kennethjpriester@gmail.com>
2017-12-11 20:11:59 -0600

    started on the implementation

--- lib/shapeways.rb
+++ lib/shapeways.rb
@@ -1,5 +1,31 @@
 require "shapeways/version"
 
 module Shapeways
-  # Your code goes here...
+  class Error < RuntimeError; end
+
+  class << self
+    attr_writer :callback_url
+  end
+
+  # Make Shapeways.api_key and Shapeways.api_secret global but also local to threads
+  #
+  def self.api_key
+    Thread.current[:etsy_api_key] || @api_key
+  end
+
+  def self.api_key=(key)
+    @api_key ||= key
+    Thread.current[:etsy_api_key] = key
+  end
+
+  def self.api_secret
+    Thread.current[:etsy_api_secret] || @api_secret
+  end
+
+  def self.api_secret=(secret)
+    @api_secret ||= secret
+    Thread.current[:etsy_api_secret] = secret
+  end
+
+  PRODUCTION_HOST = 'openapi.etsy.com'
 end
